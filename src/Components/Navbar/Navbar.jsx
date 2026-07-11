72496aed9dbe614d44ce5f554d8857c91e432a5d
Abdul Hanan <ah24620055@gmail.com>
2026-07-11 17:49:24 +0500

    Refactor Navbar component for improved structure and readability

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import { NavLink, Link } from "react-router-dom";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import carticon from "../Assets/cart_icon.png";
 
 export const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+
   return (
     <div className="navbar">
       <div className="nav-logo">
@@ -12,48 +15,32 @@ export const Navbar = () => {
       </div>
 
       <ul className="nav-menu">
-        <li>
-          <NavLink to="/" className="nav-link">
-            {({ isActive }) => (
-              <>
-                Shop
-                {isActive ? <hr /> : null}
-              </>
-            )}
-          </NavLink>
+        <li onClick={() => setMenu("shop")}>
+          <Link to="/" className="nav-link">
+            Shop
+          </Link>
+          {menu === "shop" ? <hr /> : <></>}
         </li>
 
-        <li>
-          <NavLink to="/men" className="nav-link">
-            {({ isActive }) => (
-              <>
-                Men
-                {isActive ? <hr /> : null}
-              </>
-            )}
-          </NavLink>
+        <li onClick={() => setMenu("men")}>
+          <Link to="/men" className="nav-link">
+            Men
+          </Link>
+          {menu === "men" ? <hr /> : <></>}
         </li>
 
-        <li>
-          <NavLink to="/women" className="nav-link">
-            {({ isActive }) => (
-              <>
-                Women
-                {isActive ? <hr /> : null}
-              </>
-            )}
-          </NavLink>
+        <li onClick={() => setMenu("women")}>
+          <Link to="/women" className="nav-link">
+            Women
+          </Link>
+          {menu === "women" ? <hr /> : <></>}
         </li>
 
-        <li>
-          <NavLink to="/kids" className="nav-link">
-            {({ isActive }) => (
-              <>
-                Kids
-                {isActive ? <hr /> : null}
-              </>
-            )}
-          </NavLink>
+        <li onClick={() => setMenu("kids")}>
+          <Link to="/kids" className="nav-link">
+            Kids
+          </Link>
+          {menu === "kids" ? <hr /> : <></>}
         </li>
       </ul>
 

79aca9e0a6e6c067eca1fdea3cb2cdc86d7ac176
Abdul Hanan <ah24620055@gmail.com>
2026-07-11 18:26:10 +0500

    Refactor App component to use HashRouter for improved routing consistency

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Navbar } from "./Components/Navbar/Navbar";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { Shop } from "./Pages/Shop";
 import { ShopCategory } from "./Pages/ShopCategory";
 import { Product } from "./Pages/Product";
@@ -13,23 +13,21 @@ import kids_banner from "./Components/Assets/banner_kids.png";
 
 function App() {
   return (
-    <>
-      <BrowserRouter basename="/shopper" >
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Shop />} />
-          <Route path="/men" element={<ShopCategory banner={men_banner} category="men" />} />
-          <Route path="/women" element={<ShopCategory banner={women_banner} category="women" />} />
-          <Route path="/kids" element={<ShopCategory banner={kids_banner} category="kid" />} />
-          <Route path="/product" element={<Product />} >
-          <Route path=":productid" element={<Product />} />
-          </Route>
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/login" element={<LoginSignup />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
+    <Router basename="/shopper" >
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/men" element={<ShopCategory banner={men_banner} category="men" />} />
+        <Route path="/women" element={<ShopCategory banner={women_banner} category="women" />} />
+        <Route path="/kids" element={<ShopCategory banner={kids_banner} category="kid"/>} />
+        <Route path="/product" element={<Product />} >
+        <Route path=":productid" element={<Product />} />
+        </Route>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<LoginSignup />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 

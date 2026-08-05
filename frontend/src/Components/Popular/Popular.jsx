a0808e6830cea650b1a4eaee537535366674373b
Abdul Hanan <ah24620055@gmail.com>
2026-08-05 14:58:47 +0500

    feat: add Popular component and wrap App with ShopContextProvider in main.jsx

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -8,6 +8,7 @@ export const Popular = () => {
   const[popularProducts,setPopularProducts] = useState([]);
 
   useEffect(()=>{
+    console.log(import.meta.env.VITE_API_URL)
     fetch(`${import.meta.env.VITE_API_URL}/popularinwomen`)
     .then((response)=>response.json())
     .then((data)=>setPopularProducts(data))
